c095df5dc4a79554ca9306ac2c98cb5d393c68bd
gailees <gailees@umich.edu>
2014-12-28 23:57:24 -0500

    initializing FunFacts app with playground

---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,27 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+//someConstant = 15
+
+//var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+//fruitsArray.append(10)
+fruitsArray.append("bananas")
+fruitsArray
+
+//Optionals 
+//either has a constant value or nil
+
+var optionalString: String? = "Hello"
+optionalString!
+
+
